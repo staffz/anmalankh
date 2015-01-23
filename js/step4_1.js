ab4a6ee6820c004a5d11631661fece77f801d718
staffz <staffan@monkeydancers.com>
2015-01-23 11:29:04 +0100

    Begärda fixar från Fredrik

--- js/step4_1.js
+++ js/step4_1.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     var text_max = 2500;
-    $('#commInputFeedback').html(text_max + ' tecken kvar');
+    $('#commInputFeedback').html(text_max);
 
     $('#commInput').keyup(function() {
         var text_length = $('#commInput').val().length;
         var text_remaining = text_max - text_length;
 
-        $('#commInputFeedback').html(text_remaining + ' tecken kvar');
+        $('#commInputFeedback').html(text_remaining);
     });
 });
