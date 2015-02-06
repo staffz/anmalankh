3443b84839050d0f8c82fa48a3c6acbdbb9030fb
staffz <staffan@monkeydancers.com>
2015-02-06 21:14:02 +0100

    Javascript to block erroneous changes

--- js/step4_4.js
+++ js/step4_4.js
@@ -1,3 +1,10 @@
+
+var boxesToCheck = {
+	'#intaglioCheckbox' : 'IntaglioTerm',
+	'#lithographyCheckbox' : 'LithographyTerm',
+	'#ScreenPrintCheckbox' : 'ScreenPrintTerm'
+};
+
 $(document).ready(function() {
    
     $('input[name=direction]').change(function(rb) {
@@ -7,4 +14,33 @@ $(document).ready(function() {
 				$('.ansokan-detail-row').slideUp();
    		}
     });
+
+    $.each(boxesToCheck, function(key, value){
+			$(key).change(function(rb) {
+				if(! checkIfCheckAllowed()){
+					$(this).prop('checked', false);
+				}
+	   		if($(this).is(':checked')){
+	   			$('input[name=' + value + ']').prop('disabled', false);
+				} else {
+	   			$('input[name=' + value + ']').prop('disabled', true);
+	   			$('input[name=' + value + ']').prop('checked', false);
+				}
+			});
+    });
+
 });
+
+function checkIfCheckAllowed(){
+	var count = 0;
+  $.each(boxesToCheck, function(key, value){
+		if($(key).is(':checked')){
+			count += 1;
+		}
+	});
+	if(count > 2){
+		return false;
+	} else {
+		return true;
+	}
+}
